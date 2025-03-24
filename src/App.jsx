35692cd0262c00aadb14836fbf91bdbbbf31b878
josethgatica <josethgatica@gmail.com>
2025-03-24 13:28:16 -0600

    segundo

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
-import { useState } from "react";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./views/Login";
 import Inicio from "./views/Inicio";
+import Clientes from "./views/clientes";
 import './App.css';
+import Encabezado from "./components/Encabezado/encabezado";
+import Producto from "./views/productos";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-
-        <Route path="/" element={<Login />} />
-        <Route path="/inicio" element={<Inicio />} />
-
-      </Routes>
+      <Router>
+        //Contenedor principal con margen superior
+      <main className="margen-superior-main">
+      <Encabezado/>    
+        <Routes>
+         //Definicion de rutas
+          <Route path="/" element={<Login />} />
+          <Route path="/inicio" element={<Inicio />} />
+          <Route path="/clientes" element={<Clientes/>} />
+          <Route path="/productos" element={<Producto/>} />
+        </Routes>
+      </main>
     </Router>
   );
 };
